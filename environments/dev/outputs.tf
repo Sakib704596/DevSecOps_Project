3dce06f2af3f87ad1030064937a9057109258062
Sakib Khan <khsakib2005@gmail.com>
2026-08-07 21:06:14 +0530

    Phase 1b: IAM roles for EKS cluster and node group

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -13,3 +13,10 @@ output "private_subnet_ids" {
 output "nat_gateway_ip" {
   value = module.vpc.nat_gateway_ip
 }
+output "cluster_role_arn" {
+  value = module.iam.cluster_role_arn
+}
+
+output "node_role_arn" {
+  value = module.iam.node_role_arn
+}
